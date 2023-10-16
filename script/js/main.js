5feecddf4cf21c5c0165f0c2068642084c98e1c4
adrhmdlz <adrianalzidan35@gmail.com>
2023-10-16 15:05:44 +0700

    update: Mengedit, menambahkan script baru untuk parallax effect.

--- script/js/main.js
+++ script/js/main.js
@@ -11,3 +11,44 @@ $('.page-scroll').on('click', function(event) {
 
     event.preventDefault()
 });
+
+// Parallax
+// Ketika windows di load
+$(window).on('load', function() {
+    $('.about-left').addClass('show');
+    $('.about-right').addClass('show');
+});
+
+// Ketika di scroll
+$(window).scroll(function() {
+    var windowScroll = $(this).scrollTop();
+
+    // Jumbotron
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ windowScroll/5 +'%)'
+    });
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ windowScroll/4 +'%)'
+    });
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ windowScroll/3 +'%)'
+    });
+
+    // Portfolio
+    if(windowScroll > $('.portfolio').offset().top - 250) {
+        $('.portfolio .card').each(function(i) {
+            setTimeout(function() {
+                $('.portfolio .card').eq(i).addClass('show');
+            }, 500 * (i+1));
+        });
+    }
+
+    // Certificate
+    if(windowScroll > $('.certification').offset().top - 250) {
+        $('.certification .card').each(function(i) {
+            setTimeout(function() {
+                $('.certification .card').eq(i).addClass('show');
+            }, 500 * (i+1));
+        });
+    }
+});
