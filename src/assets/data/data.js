e4d30b97488bec23a5950c43ee717051ed45c11c
adrhmdlz <adrianalzidan35@gmail.com>
2024-01-20 23:59:28 +0700

    update: Update id, img source, title, and desc for Certificate and Portfolio.

---
+++ src/assets/data/data.js
@@ -0,0 +1,58 @@
+export const cert = [
+    {
+        id: 1,
+        image: './assets/img/cert-1.png',
+        title: 'Belajar Dasar Pemrograman Web',
+        desc: '',
+    },
+    {
+        id: 2,
+        image: './assets/img/cert-2.png',
+        title: 'Belajar Membuat Front-End Web untuk Pemula',
+        desc: '',
+    },
+    {
+        id: 3,
+        image: './assets/img/cert-3.png',
+        title: 'Belajar Fundamental Front-End Web Development',
+        desc: '',
+    },
+];
+
+export const port = [
+    {
+        id: 1,
+        image: './assets/img/project-1.jpg',
+        title: 'ABlog',
+        desc: '',
+        source: '',
+    },
+    {
+        id: 2,
+        image: './assets/img/project-2.jpg',
+        title: 'Bookshelf Apps',
+        desc: '',
+        source: '',
+    },
+    {
+        id: 3,
+        image: './assets/img/project-3.jpg',
+        title: 'Dicoding Landing Page',
+        desc: '',
+        source: '',
+    },
+    {
+        id: 4,
+        image: './assets/img/project-4.jpg',
+        title: 'Server Dashboard',
+        desc: '',
+        source: '',
+    },
+    {
+        id: 5,
+        image: './assets/img/project-5.jpg',
+        title: 'Daily Dua Apps',
+        desc: '',
+        source: '',
+    },
+];
